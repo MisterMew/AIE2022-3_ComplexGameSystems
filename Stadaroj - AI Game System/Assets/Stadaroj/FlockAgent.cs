e101d5828b5d841f119c8fc4bac91ec3942a6791
MisterMew <thebesteccentric@gmail.com>
2022-05-09 16:55:48 +1000

    Monday 09.05.2022 @ 16:55 PM - Implemented Flock Spawning
    + Added script: FlockAgent.cs
    + Added script: FlockBehaviour.cs
    + Added script: Flocking.cs
    
    ~ Modified script: FlockBehaviour.cs
       + Added public abstract method: "abstract Vector3 CalculateMove(FlockAgent agent, List<Transform> transform, Flocking flock);"
    
    ~ Modified script: FlockAgent.cs
       + Added collider variables, Start method, and  void Move(Vector3 velocity) method
       + Script now requires Collider type "[RequireComponent(typeof(Collider))]"
    
    ~ Modified script: Flocking.cs
       + Added variables, Start Method, Update Method, List<Transform> FindNearbyObjects(FlockAgent agent) method and float Magnitude(float value) { return value * value; } method
    
    ~ Modified all scripts to have summaries and comments

--- Stadaroj - AI Game System/Assets/Stadaroj/FlockAgent.cs
+++ Stadaroj - AI Game System/Assets/Stadaroj/FlockAgent.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class FlockAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Collider agentCollider;
+    public Collider AgentCollider { get { return agentCollider; } } //Public accessor for agentCollider
+
+
+    /// <summary>
+    /// Start is called before the first frame update 
+    /// </summary>
     void Start()
     {
-        
+        agentCollider = GetComponent<Collider>(); //Cache the current agents collider
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="velocity">Offset position for agent to move to.</param>
+    public void Move(Vector3 velocity)
     {
-        
+        transform.forward = velocity; //Apply forward velocity
+        transform.position += velocity * Time.deltaTime; //Apply constant movement regardless of framerate
     }
 }
---
+++ Stadaroj - AI Game System/Assets/Stadaroj/Flocking.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach this script to a gameObject where you would like the flock to spawn.
+/// 
+/// This script handles the flocking behaviours of agents within a scene.
+/// </summary>
+public class Flocking : MonoBehaviour
+{
+    /* Variables */
+    public FlockAgent agentPrefab;
+    private List<FlockAgent> agents = new List<FlockAgent>();
+    public FlockBehaviour behaviour;
+
+    [Header("Agent Spawning")]
+    [Range(0F, 500F)]
+    public int agentSpawnCount = 10;
+    const float agentDensity = 0.1F;
+
+    [Header("Agent Behaviours")]
+    [Range(0F, 100F)]
+    public float acceleration = 10F;
+    [Range(0F, 100F)]
+    public float maxSpeed = 5F;
+    [Range(1F, 10F)]
+    public float neighbourRadius = 1.5F;
+    [Range(0F, 1F)]
+    public float avoidanceRadiusMultiplier = 0.5F;
+
+    /* Utility Variables */
+    private float squareMaxSpeed;
+    private float squareNeighbourRadius;
+    private float squareAvoidanceRadius;
+    public float SquareAvoidanceRadius { get { return squareAvoidanceRadius; } }
+
+
+    /// <summary>
+    /// Start is called before the first frame update 
+    /// </summary>
+    void Start()
+    {
+        squareMaxSpeed = Magnitude(squareMaxSpeed);
+        squareNeighbourRadius = Magnitude(squareNeighbourRadius);
+        squareAvoidanceRadius = Magnitude(squareAvoidanceRadius);
+
+        /* Spawn Agents */
+        for (int i = 0; i < agentSpawnCount; i++)
+        {
+            FlockAgent newAgent = Instantiate(
+                agentPrefab, 
+                Random.insideUnitSphere * agentSpawnCount * agentDensity,    //Random position within a sphere
+                Quaternion.Euler(Vector3.forward * Random.Range(0F, 360F)), //Random Rotation
+                transform //Parent
+                );
+
+            newAgent.name = "Agent" + i;
+            agents.Add(newAgent);
+        }
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        foreach(FlockAgent agent in agents) //Iterate through each agent in the list of agents
+        {
+            List<Transform> context = FindNearbyObjects(agent);
+
+            //DEMO ONLY
+            agent.GetComponentInChildren<Renderer>().material.color = Color.Lerp(Color.white, Color.red, context.Count / 6F);
+
+            //Vector3 move = behaviour.CalculateMove(agent, context, this);
+            //move *= acceleration;
+            //if (move.sqrMagnitude > squareMaxSpeed)
+            //{
+            //    move = move.normalized * maxSpeed; //Reset to be exactly at the maximum speed
+            //}
+            //agent.Move(move);
+        }
+    }
+
+    /* Utility Functions */
+    /// <summary>
+    /// Find and get all nearby objects (agents and obstacles) for each individual agent.
+    /// </summary>
+    /// <param name="agent"></param>
+    /// <returns></returns>
+    private List<Transform> FindNearbyObjects(FlockAgent agent)
+    {
+        List<Transform> context = new List<Transform>();
+        Collider[] contextColliders = Physics.OverlapSphere(agent.transform.position, neighbourRadius);
+
+        foreach(Collider c in contextColliders) {
+            if (c != agent.AgentCollider) //Saftey check for the current agents collider
+            {
+                context.Add(c.transform);
+            }
+        }
+        return context;
+    }
+
+    /* Maths Functions */
+    /// <summary>
+    /// Calculate the square root of a floating point variable
+    /// </summary>
+    /// <param name="value">Target value to perform sqrt operation.</param>
+    /// <returns>The magnitude of the pass  value.</returns>
+    private float Magnitude(float value) { return value * value; }
+}
