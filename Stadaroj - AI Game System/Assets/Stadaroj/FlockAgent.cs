a30053e0be283a0ecc3725d4b2d90d251641c439
MisterMew <thebesteccentric@gmail.com>
2022-05-09 10:02:10 +1000

    Update

--- Stadaroj - AI Game System/Assets/Stadaroj/FlockAgent.cs
+++ Stadaroj - AI Game System/Assets/Stadaroj/FlockAgent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoidAgent : MonoBehaviour
+public class FlockAgent : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
